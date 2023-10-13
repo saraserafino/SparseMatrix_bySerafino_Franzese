f2e4833d54c2a3b8ff95e6907de4ff2713c75404
Sara <146431491+saraserafino@users.noreply.github.com>
2023-10-13 22:08:09 +0200

    Update SparseMatrixCOO.cpp

---
+++ src/SparseMatrixCOO.cpp
@@ -0,0 +1,9 @@
+#include "SparseMatrixCOO.hpp"
+#include <vector>
+
+// the array values contains all the nonzero values
+std::vector<double> values =
+// the array rows of int contains their corresponding row indices
+std::vector<int> rows = 
+// the array cols of int contains their corresponding column indices
+std::vector<int> cols =
