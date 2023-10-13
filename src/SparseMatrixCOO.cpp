f82b279e20e35662451631632cb9bba87b3a3458
Sara <146431491+saraserafino@users.noreply.github.com>
2023-10-13 22:13:44 +0200

    Update SparseMatrixCOO.cpp

--- src/SparseMatrixCOO.cpp
+++ src/SparseMatrixCOO.cpp
@@ -1,6 +1,7 @@
 #include "SparseMatrixCOO.hpp"
 #include <vector>
 
+// the length of the following three arrays is nnz (number of non-zeros)
 // the array values contains all the nonzero values
 std::vector<double> values =
 // the array rows of int contains their corresponding row indices
