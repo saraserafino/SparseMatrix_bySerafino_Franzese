b0514dc229190601fbdc614846c78d637fcc3082
Sara <146431491+saraserafino@users.noreply.github.com>
2023-10-13 21:13:21 +0200

    Update converter.hpp

--- converter.hpp
+++ converter.hpp
@@ -2,5 +2,11 @@
 #define CONVERTER_HPP_
 #include <vector>
 
+namespace stat {
+  // Converter from COO format to CSR
+  auto converter_COO_to_CSR();
+  // Converter from CSR format to COO
+  auto converter_CSR_to_COO();
+  }
 
 #endif
