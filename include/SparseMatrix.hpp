478d3e1d818ba75d50d5412b3b74de39e534765a
Sara Serafino <serafinos1999@gmail.com>
2023-10-13 21:25:52 +0200

    Update include

---
+++ include/SparseMatrix.hpp
@@ -0,0 +1,6 @@
+#ifndef SPARSE_MATRIX_HPP_
+#define SPARSE_MATRIX_HPP_
+#include <vector>
+
+
+#endif
---
+++ include/SparseMatrixCOO.hpp
@@ -0,0 +1,6 @@
+#ifndef SPARSE_MATRIX_COO_HPP_
+#define SPARSE_MATRIX__COO_HPP_
+#include <vector>
+
+
+#endif
