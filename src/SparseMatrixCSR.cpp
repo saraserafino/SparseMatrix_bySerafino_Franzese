46e2ab845e2641f9e38ee80befbb87eb1f2ad944
Sara <146431491+saraserafino@users.noreply.github.com>
2023-10-13 22:13:06 +0200

    Update SparseMatrixCSR.cpp

---
+++ src/SparseMatrixCSR.cpp
@@ -0,0 +1,14 @@
+#include "SparseMatrixCSR.hpp"
+#include <vector>
+
+// the array values contains all the nonzero values
+// we'll call the standard length nnz (nonzero)
+std::vector<double> values =
+// the array columns of int contains their corresponding column indices
+// so its length is the same as that of values (nnz)
+std::vector<int> columns =
+// the array rows_idx of int contains the cumulative number of nonzero entries up to the
+// i-th row excluded. Its length is m+1 with m number of rows of A
+std::vector<int> rows_idx = 
+row_idx[0] = 0 // convention. In this way row_idx[i+1] - row_idx[i] represents the
+// number of nonzero elements in the i-th row
