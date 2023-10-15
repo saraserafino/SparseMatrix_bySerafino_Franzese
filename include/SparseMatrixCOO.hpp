5733187667a620c3fda06670b51135906e0c79b2
Sara Serafino <serafinos1999@gmail.com>
2023-10-15 19:36:26 +0200

    COO

--- include/SparseMatrixCOO.hpp
+++ include/SparseMatrixCOO.hpp
@@ -1,6 +1,25 @@
 #ifndef SPARSE_MATRIX_COO_HPP_
 #define SPARSE_MATRIX__COO_HPP_
-#include <vector>
+// #include <vector>
 
+class SpareMatrixCOO : public SparseMatrix {
+public:
+// per ognuno dei punti richiesti (get number of rows, columns, read an entry etc) dobbiamo fare una funzione come
+    /*
+    int get_rows() override {
+
+    }
+    */
+   SparseMatrixCOO(const int &input_size_rows, const int &input_size_columns, int nnz);
+   void setElement(int index_row, int index_col, double value);
+   void printCOO();
+
+
+private:
+// the length of the following array is nnz (number of non-zeros)
+// the array rows of int contains their corresponding rows indices
+std::vector<int> rows;
+int current_nnz;
+};
 
 #endif
