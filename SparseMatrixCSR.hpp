1a3aa2c3b2de5f0c1c1032a9603a856bf87682c2
Sara <146431491+saraserafino@users.noreply.github.com>
2023-10-23 17:55:28 +0200

    Update SparseMatrixCSR.hpp

--- SparseMatrixCSR.hpp
+++ SparseMatrixCSR.hpp
@@ -1,19 +1,17 @@
 #ifndef SPARSE_MATRIX_CSR_HPP_
 #define SPARSE_MATRIX_CSR_HPP_
+
 #include "SparseMatrix.hpp"
 #include <vector>
 
 class SpareMatrixCSR : public SparseMatrix {
 public:
-// per ognuno dei punti richiesti (get number of rows, columns, read an entry etc) dobbiamo fare una funzione come
-    /* int get_rows() override {
+   SparseMatrixCSR(std::vector<double>& values, std::vector<unsigned int>& rows, std::vector<unsigned int>& columns);
+   unsigned int get_num_rows() const override;
+   // Redefining the operator () in both const and not-const version
+   double& operator()(unsigned int row_idx, unsigned int col_idx) override;
+   double operator()(unsigned int row_idx, unsigned int col_idx) const override;
 
-    }
-    */
-   SparseMatrixCSR(const int &input_size_rows, const int &input_size_columns, int nnz);
-
-    void setElement(int index_row, int index_col, double value);
-    // void printmatrix(); // essendo già in SparseMatrix non penso serva definirlo qua
 private:
 // the array rows_idx of int contains the cumulative number of nonzero entries up to the
 // i-th row excluded. Its length is m+1 with m number of rows of A
