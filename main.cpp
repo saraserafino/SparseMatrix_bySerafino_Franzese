79ac4f47be2e7cfbb30c766c68a07931e4a0ef4a
your_username <email@yourdomainname.com>
2023-10-23 16:48:54 +0200

    ho aggiunto una cosa che stampi il numero di elementi non nulli

--- main.cpp
+++ main.cpp
@@ -7,7 +7,8 @@ int main(){
   std::vector<unsigned int> columns = {0,2,4};
   std::vector<unsigned int> rows = {1,0,3};
   SparseMatrixCOO m(values, rows, columns);
-  std::cout << "matrix m with " << m.get_num_rows() << "rows and " << m.get_num_columns() << " columns\n";
+  std::cout << "numero di elementi non nulli: " << m.get_num_nnz();
+  std::cout << "matrix m with " << m.get_num_rows() << " rows and " << m.get_num_columns() << " columns\n";
   std::cout << "m(1,0): " << m(1,0) << std::endl;
   m(2,2) = 100;
   std::cout << "Dopo m(2,2) = 100, cout di m(2,2) da: " << m(2,2) << std::endl;
