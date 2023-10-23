c8104b383c6304a8dcb57b6dd6df4e4e68006c30
Sara <146431491+saraserafino@users.noreply.github.com>
2023-10-23 17:54:58 +0200

    Update SparseMatrixCOO.hpp

--- SparseMatrixCOO.hpp
+++ SparseMatrixCOO.hpp
@@ -9,8 +9,8 @@ public:
     SparseMatrixCOO(std::vector<double>& values, std::vector<unsigned int>& rows, std::vector<unsigned int>& columns);
     unsigned int get_num_rows() const override;
     // Redefining the operator () in both const and not-const version
-    double& operator()(int row_idx, int col_idx) override; // overload secondo il suo metodo di salvare i valori
-    double operator()(int row_idx, int col_idx) const override;
+    double& operator()(unsigned int row_idx, unsigned int col_idx) override; // overload secondo il suo metodo di salvare i valori
+    double operator()(unsigned int row_idx, unsigned int col_idx) const override;
 
 private:
 // the array rows contains the row indices
