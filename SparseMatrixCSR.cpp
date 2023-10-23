9b80280967c3ccdfc5f294e6ae96599aff1af588
Sara <146431491+saraserafino@users.noreply.github.com>
2023-10-23 17:55:14 +0200

    Update SparseMatrixCSR.cpp

--- SparseMatrixCSR.cpp
+++ SparseMatrixCSR.cpp
@@ -1,21 +1,51 @@
 #include "SparseMatrixCSR.hpp"
 #include <vector>
 
-class SparseMatrixCSR : public SparseMatrix {
-public:
-// per ognuno dei punti richiesti (get number of rows, columns, read an entry etc) dobbiamo fare una funzione come
-    /* int get_rows() override {
+SparseMatrixCSR::SparseMatrixCSR(std::vector<double>& values, std::vector<unsigned int>& rows, std::vector<unsigned int>& columns)
+: SparseMatrix(values,columns), rows(rows) {}
+    
+unsigned int SparseMatrixCOO::get_num_rows() const { 
+  if (values.empty()) {
+    return 0; // If there are no values, there is no row
+  }
+  unsigned int max = rows[0];
+  for(auto row_idx : rows)
+    if(row_idx > max)
+      max = row_idx;
+  return max + 1;
+}
 
-    }
-    */
-   SparseMatrixCSR(const int &input_size_rows, const int &input_size_columns, int nnz)
-    : size_rows(input_size_rows), size_columns(input_size_columns), nnz(nnz)
-    {
-        values.resize(nnz);
-        cols.resize(nnz);
+double& SparseMatrixCOO::operator()(unsigned int row_idx, unsigned int col_idx) {
+  if(row_idx >= this->get_num_rows() || col_idx >= this->get_num_columns()) {
+    throw std::out_of_range ("Indexes out of range");
+  }
+
+  for(int i = 0; i < values.size(); ++i)
+    if(rows[i] == row_idx && columns[i] == col_idx) // if the indeces are present in the vectors rows and columns
+      return values[i]; // it returns the value
+  // otherwise it adds it and returns the new value
+  rows.push_back(row_idx);
+  columns.push_back(col_idx);
+  values.push_back(0.0); // default value of 0 that will be changed later
+  return values.back();
+}
+
+// Same as above but since now it's const, it doesn't return the reference but the value
+double SparseMatrixCOO::operator()(unsigned int row_idx, unsigned int col_idx) const {
+  if(row_idx >= this->get_num_rows() || col_idx >= this->get_num_columns()) {
+    throw std::out_of_range ("Indexes out of range");
+  }
+
+  for(int i = 0; i < values.size(); ++i)
+    if(rows[i] == row_idx && columns[i] == col_idx)
+      return values[i];
+
+  return 0.0;
+}
+
+
         row_idx.resize(size_rows + 1, 0); // initialize row_idx with 0
         current_nnz = 0;
-    }
 
     void setElement(int index_row, int index_col, double value) {
         if (index_row < 0 || index_row >= size_rows || index_col < 0 || index_col >= size_columns)
@@ -52,8 +82,3 @@ public:
         }
         std::cout << row_idx[index_row] << "]" << std::endl;
     }
-private:
-// the array rows_idx of int contains the cumulative number of nonzero entries up to the
-// i-th row excluded. Its length is m+1 with m number of rows of A
-std::vector<int> rows_idx;
-};
