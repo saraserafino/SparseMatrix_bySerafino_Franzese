18417dddacc0d194eb71352ecac8cee4c73e3493
Sara Serafino <serafinos1999@gmail.com>
2023-10-29 22:40:37 +0100

    DAJE

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -x
 mkdir -p build
-g++ -Iinclude/ src/SparseMatrix.cpp -c -o build/SparseMatrix.o
-g++ -Iinclude/ src/SparseMatrixCOO.cpp -c -o build/SparseMatrixCOO.o
-g++ -Iinclude/ src/SparseMatrixCSR.cpp -c -o build/SparseMatrixCSR.o
-g++ -Iinclude/ src/main.cpp -c -o build/main.o
+g++ -std=c++17 -Iinclude/ src/SparseMatrix.cpp -c -o build/SparseMatrix.o
+g++ -std=c++17 -Iinclude/ src/SparseMatrixCOO.cpp -c -o build/SparseMatrixCOO.o
+g++ -std=c++17 -Iinclude/ src/SparseMatrixCSR.cpp -c -o build/SparseMatrixCSR.o
+g++ -std=c++17 -Iinclude/ src/main.cpp -c -o build/main.o
 g++ -std=c++17 -Wall -Wpedantic build/SparseMatrix.o build/SparseMatrixCOO.o build/SparseMatrixCSR.o build/main.o -o build/HomeworkSF
 set +x
 if [ $? -eq 0 ]; then
