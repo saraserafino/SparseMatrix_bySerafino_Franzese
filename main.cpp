9478414007f2e6a48d0f5f2993f580bfcb47ff99
Sara Serafino <serafinos1999@gmail.com>
2023-10-29 14:28:54 +0100

    Sistemato il main

--- main.cpp
+++ main.cpp
@@ -11,38 +11,37 @@ int main(){
   std::cout << "The matrix m has " << m.get_num_rows() << " rows, " << m.get_num_columns()
   << " columns and " << m.get_num_nnz() << " non-zero values." << std::endl;
   
-  std::cout << "m(1,0) = " << m(1,0) << std::endl;
   std::cout << "m(2,3) = " << m(2,3) << std::endl;
-  m(1,0) = 6.9; // non-const version
-  //m(2,3) = 5.7;
-  std::cout << "Changing their value to" << std::endl;
-  std::cout << "m(1,0) = " << m(1,0) << std::endl;
+  m(2,3) = 5.7; // non-const version
+  std::cout << "Changing its value to" << std::endl;
   std::cout << "m(2,3) = " << m(2,3) << std::endl;
 
   m.print_matrix();
   m.convert(); // convert the matrix from COO to CSR
   m.print_dense_matrix();
 
-  std::vector<double> vettore(m.get_num_columns(),1);
-  std::vector<double> vettore2(m.get_num_columns(),0);
-  vettore2[3]=1;
-  auto q= m*vettore;
+  std::vector<double> vector_ones(m.get_num_columns(),1);
+  auto q = m * vector_ones;
 
-  std::cout << "Array elements: ";
-    for (int i = 0; i < m.get_num_rows(); ++i) {
-        std::cout <<q[i] << " ";
-    }
-    std::cout << std::endl;
+  std::cout << "The multiplication of the matrix for a vector of ones"
+            << " gives the sum of the rows of the matrix: " << std::endl;
+  for (int i = 0; i < m.get_num_rows(); ++i) {
+      std::cout << q[i] << " ";
+  }
+  std::cout << std::endl;
 
-    auto q2= m*vettore2;
+  std::vector<double> vector_canonical(m.get_num_columns(),0);
+  vector_canonical[3] = 1;
+  auto q2 = m * vector_canonical;
 
-  std::cout << "Array elements: ";
-    for (int i = 0; i < m.get_num_rows(); ++i) {
-        std::cout <<q2[i] << " ";
-    }
-    std::cout << std::endl;
-
-
+  std::cout << "The multiplication of the matrix for the i-th vector of the"
+            << " canonical basis gives the i-th column of the matrix." << std::endl
+            << "For example the 3th vector of the canonical basis"
+            << " gives the 3th column of the matrix:" << std::endl;
+  for (int i = 0; i < m.get_num_rows(); ++i) {
+      std::cout << q2[i] << " ";
+  }
+  std::cout << std::endl;
 
   // Be aware when defining row_idx for CSR: the first element must be 0 by convention
   std::vector<unsigned int> row_idx = {0, 2, 4, 4, 6}; // CSR
@@ -50,12 +49,9 @@ int main(){
   std::cout << "The matrix m2 has " << m2.get_num_rows() << " rows, " << m2.get_num_columns()
   << " columns and " << m2.get_num_nnz() << " non-zero values." << std::endl;
 
-  std::cout << "m2(1,0) = " << m2(1,0) << std::endl;
   std::cout << "m2(2,3) = " << m2(2,3) << std::endl;
-  m2(1,0) = 6.9; // non-const version
-  //m2(2,3) = 5.7;
-  std::cout << "Changing their value to" << std::endl;
-  std::cout << "m2(1,0) = " << m2(1,0) << std::endl;
+  m2(2,3) = 5.7; // non-const version
+  std::cout << "Changing its value to" << std::endl;
   std::cout << "m2(2,3) = " << m2(2,3) << std::endl;
 
 
@@ -63,21 +59,23 @@ int main(){
   m2.convert(); // convert the matrix from CSR to COO
   m2.print_dense_matrix();
   
-  auto q3= m2*vettore;
+  auto q3 = m2 * vector_ones;
+  std::cout << "The multiplication of the matrix for a vector of ones"
+            << " gives the sum of the rows of the matrix: " << std::endl;
+  for (int i = 0; i < m2.get_num_rows(); ++i) {
+      std::cout << q3[i] << " ";
+  }
+  std::cout << std::endl;
 
-  std::cout << "Array elements: ";
-    for (int i = 0; i < m2.get_num_rows(); ++i) {
-        std::cout <<q3[i] << " ";
-    }
-    std::cout << std::endl;
-
-    auto q4= m2*vettore2;
-std::cout << "Array elements: ";
-    for (int i = 0; i < m2.get_num_rows(); ++i) {
-        std::cout <<q4[i] << " ";
-    }
-    std::cout << std::endl;
-
+  auto q4 = m2 * vector_canonical;
+  std::cout << "The multiplication of the matrix for the i-th vector of the"
+            << " canonical basis gives the i-th column of the matrix." << std::endl
+            << "For example the 3th vector of the canonical basis"
+            << " gives the 3th column of the matrix." << std::endl;
+  for (int i = 0; i < m2.get_num_rows(); ++i) {
+      std::cout << q4[i] << " ";
+  }
+  std::cout << std::endl;
   
   return 0;
 }
